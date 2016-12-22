86a582f2a00e6d0a999609bc97d54b6517bedac0
Knight.Zhou <YoungWelle@gmail.com>
2016-12-22 11:34:53 +0800

    ADDED:mesos-starter dependency

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SpringCloudMesos
-HA SpringCloud eureka service discovery
+High Available SpringCloud eureka service discovery on Mesos
+# References
+http://dockone.io/article/1702
+
+http://blog.csdn.net/zhuchuangang/article/details/51295380
---
+++ src/main/java/info/smartkit/SpringCloudMesosApplication.java
@@ -0,0 +1,13 @@
+package info.smartkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudMesosApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMesosApplication.class, args);
+	}
+}
